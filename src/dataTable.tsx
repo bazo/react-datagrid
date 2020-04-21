2f662dec22c0f3d026421dd87712dd71cc34d94d
Martin Bazik <martin@bazo.sk>
2020-04-21 11:59:33 +0200

    initial commit

---
+++ src/dataTable.tsx
@@ -0,0 +1,157 @@
+import React, { useMemo, ReactNode } from "react";
+import VirtualizedTable from "./virtualizedTable";
+import { Column, TableCellProps } from "react-virtualized";
+
+interface Columns {
+	[key: string]: string;
+}
+interface ColumnWidths {
+	[key: string]: number;
+}
+
+type CellRenderer = (value: any, row?: object) => string | ReactNode;
+type ActionRenderer = (row: object) => ReactNode;
+
+interface Props {
+	columns: Columns;
+	columnWidths?: ColumnWidths;
+	data: {
+		[K in keyof Columns]: any;
+	}[];
+	renderers?: {
+		[K in keyof Columns]: CellRenderer;
+	};
+	actions?: ActionRenderer[];
+	virtual?: boolean;
+	children?: React.ReactNode;
+	defaultColumnWidth?: number;
+}
+
+const defaultCellRenderer = ({
+	cellData,
+	columnData,
+	columnIndex,
+	dataKey,
+	isScrolling,
+	rowData,
+	rowIndex,
+}: TableCellProps): ReactNode => {
+	return cellData;
+};
+
+const wrapRenderer = (renderer: CellRenderer) => ({
+	cellData,
+	columnData,
+	columnIndex,
+	dataKey,
+	isScrolling,
+	rowData,
+	rowIndex,
+}: TableCellProps): ReactNode => renderer(cellData, rowData);
+
+const actionsRenderer = (actions: ActionRenderer[]) => ({
+	cellData,
+	columnData,
+	columnIndex,
+	dataKey,
+	isScrolling,
+	rowData,
+	rowIndex,
+}: TableCellProps): ReactNode => {
+	return actions.map(action => {
+		return action(rowData);
+	});
+};
+
+const DataTable = ({
+	columns,
+	columnWidths,
+	defaultColumnWidth = 150,
+	data,
+	renderers = {},
+	actions = [],
+	virtual = false,
+	children,
+}: Props) => {
+	const columnEntries = useMemo(() => Object.entries(columns), [columns]);
+
+	const rowGetter = ({ index }: { index: number }) => {
+		return data[index];
+	};
+
+	if (virtual) {
+		let columns: React.ReactElement<any, typeof Column>[] = [];
+
+		if (!children) {
+			columns = columnEntries.map(([key, title]) => {
+				return (
+					<Column
+						key={key}
+						dataKey={key}
+						label={title}
+						headerClassName={`header ${key}`}
+						className={`cell ${key}`}
+						width={columnWidths && columnWidths[key] ? columnWidths[key] : defaultColumnWidth}
+						cellRenderer={renderers[key] ? wrapRenderer(renderers[key]) : defaultCellRenderer}
+					/>
+				);
+			});
+
+			if (actions.length > 0)
+				columns.push(
+					<Column
+						key="actions"
+						dataKey="actions"
+						label="Actions"
+						headerClassName={`header actions`}
+						className={`cell actions`}
+						width={200 + (actions.length - 1) * 50}
+						cellRenderer={actionsRenderer(actions)}
+						disableSort={true}
+					/>
+				);
+		}
+
+		return (
+			<VirtualizedTable rowCount={data.length} rowGetter={rowGetter} rowHeight={20}>
+				{children ? children : columns}
+			</VirtualizedTable>
+		);
+	}
+
+	return (
+		<table className="table table-striped table-hover table-sm">
+			<thead className="thead-dark">
+				<tr>
+					{columnEntries.map(([key, title]) => {
+						return <th key={key}>{title}</th>;
+					})}
+					{actions.length > 0 && <th>Actions</th>}
+				</tr>
+			</thead>
+
+			<tbody>
+				{data.map((row, index) => {
+					return (
+						<tr key={index}>
+							{columnEntries.map(([key, title]) => {
+								const value = row[key];
+								return <td key={key}>{renderers[key] ? renderers[key](value, row) : value}</td>;
+							})}
+							{actions.length > 0 && (
+								<td>
+									{actions.map(action => {
+										return action(row);
+									})}
+									&nbsp;
+								</td>
+							)}
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
+};
+
+export default DataTable;
---
+++ src/virtualizedTable.tsx
@@ -0,0 +1,70 @@
+import React, { FC, useLayoutEffect, useRef } from "react";
+import { AutoSizer, Table, Index } from "react-virtualized";
+import "react-virtualized/styles.css";
+
+const NoRows = () => {
+	return <div>No data</div>;
+};
+
+interface Props {
+	disableHeader?: boolean;
+	headerHeight?: number;
+	rowCount: number;
+	noRowsRenderer?: () => JSX.Element | null;
+	overscanRowCount?: number;
+	rowHeight: number;
+	rowGetter: (info: Index) => any;
+	children?: React.ReactNode;
+}
+
+const rowClassName = ({ index }: { index: number }): string => {
+	if (index === -1) {
+		return "virtualized-table-row";
+	}
+
+	return `virtualized-table-row ${(index + 1) % 2 === 0 ? "even" : "odd"}`;
+};
+
+const VirtualizedTable: FC<Props> = ({
+	disableHeader = false,
+	headerHeight = 35,
+	rowCount,
+	noRowsRenderer = NoRows,
+	overscanRowCount = 10,
+	rowHeight = 25,
+	rowGetter,
+	children,
+}) => {
+	const tableHeight = useRef<number>(500);
+
+	useLayoutEffect(() => {
+		const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		tableHeight.current = height - 200;
+	}, [document.documentElement.clientHeight, window.innerHeight]);
+
+	return (
+		<AutoSizer>
+			{({ width, height }) => (
+				<Table
+					headerClassName="virtualized-table-header-row"
+					className="virtualized-table"
+					rowClassName={rowClassName}
+					disableHeader={disableHeader}
+					headerHeight={headerHeight}
+					height={tableHeight.current}
+					noRowsRenderer={noRowsRenderer}
+					estimatedRowSize={rowHeight}
+					overscanRowCount={overscanRowCount}
+					rowHeight={rowHeight}
+					rowGetter={rowGetter}
+					rowCount={rowCount}
+					width={width}
+				>
+					{children}
+				</Table>
+			)}
+		</AutoSizer>
+	);
+};
+
+export default VirtualizedTable;
